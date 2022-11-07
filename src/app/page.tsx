bad59668432175b6756a7a298e72001c6f9b47b4
Dennis vinterfjärd <dennis@vinterfjard.com>
2022-11-07 22:03:54 +0100

    prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,22 @@ import prisma from "../lib/prisma";
 import "../styles/tailwind.css";
 
 async function getHello() {
-  const link = await prisma.link.findFirst(
-    {
-        where: {
-            reference_url: "initial_commit"
-        }
-    }
-  )
-  return link?.original_data
+  const link = await prisma.link.findFirst({
+    where: {
+      reference_url: "initial_commit",
+    },
+  });
+  return link?.original_data;
 }
 
-
 export default async function Home() {
-
   const databaseItems = await getHello();
 
   console.log(databaseItems);
-  
+
   return (
     <div>
-      <h1 className='text-red-500'>hello world</h1>
+      <h1 className="text-red-500">hello world</h1>
     </div>
-  )
+  );
 }
