2238fb6c478b5cd3e1fa0c71056a4cb49fe1fd6e
Dennis vinterfjärd <dennis@vinterfjard.com>
2022-11-09 23:50:02 +0100

    Added middleware that can redirect based on if page exists in database or not

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { LinkResponse } from "./lib/LinkType";
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent){
+    const reference = req.nextUrl.pathname.split("/").pop();
+    
+    if (!reference || typeof reference !== "string"){
+        return NextResponse.rewrite(req.nextUrl.origin)
+    }
+
+    const referenceFetch = await fetch(`${req.nextUrl.origin}/api/${reference}`);
+    
+    if (referenceFetch.status !== 200) {
+        console.log("404");
+        return NextResponse.redirect(req.nextUrl.origin)
+    }
+
+    const data = await referenceFetch.json() as LinkResponse;
+
+    if (data?.link_type === "BIN" && data?.original_data){
+        return NextResponse.redirect(data.original_data);
+    }
+}
+
+export const config = {
+    matcher: '/:reference',
+  }
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+
+const Index: NextPage = () => {
+  return (
+    <div>Hello world</div>
+  );
+};
+
+export default Index;
