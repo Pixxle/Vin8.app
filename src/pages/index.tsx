1cf137a9d46c8543b4d95b6fc15bb8145d64d9b9
Dennis vinterfjärd <dennis@vinterfjard.com>
2022-11-10 23:42:04 +0100

    Sneak designing a few components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import Button from "../components/button";
 
 
 const Index: NextPage = () => {
   return (
-    <div>Hello world</div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button 
+        text="Hello World" 
+        height="4em" 
+        width="4em"
+        fontSize="5rem"
+        onClick={() => console.log("Hello World")}
+        />
+    </div>
   );
 };
 
